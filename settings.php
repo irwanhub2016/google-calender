cef6238f41d35d28054b9fac583efe9f5c0050fd
Irwan Syarifudin <irwansyarifudin16@gmail.com>
2018-11-05 08:33:24 +0700

    versi satu

---
+++ settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Google App Client Id */
+define('CLIENT_ID', 'xxxxxxxxxxxxxxxxxxxx');
+
+/* Google App Client Secret */
+define('CLIENT_SECRET', 'xxxxxxxxxxxxxxxxxxxx');
+
+/* Google App Redirect Url */
+define('CLIENT_REDIRECT_URL', 'xxxxxxxxxxxxxxxxxxxx');
+
+?>
